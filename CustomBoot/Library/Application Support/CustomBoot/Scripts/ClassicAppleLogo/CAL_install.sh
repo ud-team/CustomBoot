ed89b32138d79156816fee728fddfe74e27581b3
Flipp3rrr <philipkreuning@icloud.com>
2019-08-31 13:28:06 +0200

    Create CAL_install.sh
    Script to set up the ClassicAppleLogo

---
+++ CustomBoot/Library/Application Support/CustomBoot/Scripts/ClassicAppleLogo/CAL_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+cd "/"
+mv -f "/Library/Application Support/CustomBoot/Images/ClassicAppleLogo/apple-logo-black-n84@2x~iphone.png" "/System/Library/PrivateFrameworks/ProgressUI.framework/apple-logo-black-n84@2x~iphone.png"
+mv -f "/Library/Application Support/CustomBoot/Images/ClassicAppleLogo/apple-logo-black@2x~iphone.png" "/System/Library/PrivateFrameworks/ProgressUI.framework/apple-logo-black@2x~iphone.png"
