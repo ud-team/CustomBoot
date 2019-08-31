351692b0d0e3596b88235e82adc75e2d1f709c86
Flipp3rrr <philipkreuning@icloud.com>
2019-08-31 12:13:43 +0200

    Fixed stuff for 0.1.0

---
+++ packages.sh
@@ -0,0 +1,5 @@
+cd '/Users/gebruiker/Documents/GitHub/CustomBoot/CustomBoot/'
+find . -name '.DS_Store' -type f -delete
+
+cd '/Users/gebruiker/Documents/GitHub/CustomBoot/'
+dpkg-deb -b ClassicAppleBoot
