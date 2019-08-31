90dfd6b67f9d9cb2308c4fb3662e682317d35d47
Alex King <alexking5@protonmail.com>
2019-08-31 11:29:12 +0100

    start theos shit

---
+++ customboot/custombootprefs/CUBRootListController.m
@@ -0,0 +1,13 @@
+#include "CUBRootListController.h"
+
+@implementation CUBRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+	}
+
+	return _specifiers;
+}
+
+@end
